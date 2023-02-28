4a0b6ec15330af7ea79ccb5c5f2ec953af88edfa
dusia.tm <ppink@mail.ru>
2023-02-28 20:23:38 +0300

    Initial commit

---
+++ src/main/java/org/example/Task_4.java
@@ -0,0 +1,11 @@
+package org.example;
+
+    public class Task_4 {
+    public static void main(String[] args) throws Exception {
+        for (int i = 1; i <= 100; i++){
+            if (i % 2 == 0) {
+            System.out.println(i);
+        }
+    }
+    }
+    }
