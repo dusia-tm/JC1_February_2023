049ec73b37fd3d99997db5441ff2e44956510e76
dusia.tm <ppink@mail.ru>
2023-03-08 23:16:06 +0300

    Initial commit

--- src/main/java/org/example/Task_8_if.java
+++ src/main/java/org/example/Task_8_if.java
@@ -1,1 +1,30 @@
-git commit -i <files>
+public class Task_8_if {
+    public static void main(String[] args) {
+        for (int a = 0; a < 13; a ++)
+            if (a == 1) {
+                System.out.println("Январь");
+            } else if (a == 2) {
+                System.out.println("Февраль");
+            } else if (a == 3) {
+                System.out.println("Март");
+            } else if (a == 4) {
+                System.out.println("Апрель");
+            } else if (a == 5) {
+                System.out.println("Май");
+            } else if (a == 6) {
+                System.out.println("Июнь");
+            } else if (a == 7) {
+                System.out.println("Июль");
+            } else if (a == 8) {
+                System.out.println("Август");
+            } else if (a == 9) {
+                System.out.println("Сентябрь");
+            } else if (a == 10) {
+                System.out.println("Октябрь");
+            } else if (a == 11) {
+                System.out.println("Ноябрь");
+            } else if (a == 12) {
+                System.out.println("Декабрь");
+            }
+    }
+}
