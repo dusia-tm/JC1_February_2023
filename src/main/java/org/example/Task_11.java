a81e1449f59e6a43bb1b4fd36d0dd1fefe00c384
dusia.tm <ppink@mail.ru>
2023-03-08 18:58:25 +0300

    Initial commit

--- src/main/java/org/example/Task_11.java
+++ src/main/java/org/example/Task_11.java
@@ -5,9 +5,8 @@ import java.util.Scanner;
 public class Task_11 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите число: ");
-        int a = in.nextInt();
-        int b = in.nextInt();
-        System.out.println(Math.max(a, b));
+        System.out.print("Введите число: ");
+        int a, b = in.nextInt();
+        System.out.println(Math.max(a, b);
     }
 }
