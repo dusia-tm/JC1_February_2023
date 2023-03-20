5c4dfd73add6d05e1df5dde4b5db14651b64b42e
dusia.tm <ppink@mail.ru>
2023-03-20 19:33:18 +0300

    Initial commit

--- src/main/java/org/example/Task_4.java
+++ src/main/java/org/example/Task_4.java
@@ -2,7 +2,7 @@ package org.example;
 
     public class Task_4 {
     public static void main(String[] args) throws Exception {
-        for (int i = 1; i <= 100; i++){
+        for (int i = 0; i <= 101; i++){
             if (i % 2 == 0) {
             System.out.println(i);
              }
